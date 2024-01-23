34a2a8505a3001f4232babfaff3df72288e0f015
Rakhshan khan <rakshaankhan1@gmail.com>
2024-01-23 23:08:34 +0530

    Add Dashboard Page Components

--- src/Components/Common/dashboardPageComponents/DashboardPageComponents.jsx
+++ src/Components/Common/dashboardPageComponents/DashboardPageComponents.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import "./dashboardPageComponent.css"
 
 import messageIcon from '../../../assets/messageIcon.svg'
 
 function DashboardPageComponents(){
-    return(
-        <div className="bg-green-400">
-            <img className="h-3 w-3" src={messageIcon} alt="" />
-            <h1>Admit Card</h1>
+    return (
+      <div className="dashboardPageComponent rounded-lg">
+        <div className="flex justify-center pt-2">
+          <div className="rounded-full h-20 w-20 flex items-center justify-center bg-white">
+            <img className="h-10 w-10" src={messageIcon} alt="" />
+          </div>
         </div>
+        <h1 className="text-center pt-2 text-lg">Admit Card</h1>
+      </div>
     )
 }
 
--- src/Components/Student/StudentDashboard.jsx
+++ src/Components/Student/StudentDashboard.jsx
@@ -1,34 +1,27 @@
-import React from "react";
-import Navbar from "../Common/Navbar/Navbar";
-import StudentSideBar from "./StudentSideBar";
-import StudentMainContent from "./studentMainContent";
+import React from 'react'
+import './studentDashboard.css'
+import Navbar from '../Common/Navbar/Navbar'
+import StudentSideBar from './StudentSideBar'
+import StudentMainContent from './studentMainContent'
 
-
-function StudentDashboard(){
-    return (
-      <div style={{ display: 'flex' }}>
-        <StudentSideBar />
-        <div style={{ width: '100%' }}>
-          <Navbar />
-          <div
-            className="w-20"
-            style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(4, 1fr)',
-              gap: '10px',
-            }}
-          >
-            <StudentMainContent />
-            <StudentMainContent />
-            <StudentMainContent />
-            <StudentMainContent />
-            <StudentMainContent />
-            <StudentMainContent />
-            <StudentMainContent />
-          </div>
+function StudentDashboard() {
+  return (
+    <div style={{ display: 'flex' }}>
+      <StudentSideBar />
+      <div style={{ width: '100%' }}>
+        <Navbar />
+        <div className="dashboardContentContainer">
+          <StudentMainContent />
+          <StudentMainContent />
+          <StudentMainContent />
+          <StudentMainContent />
+          <StudentMainContent />
+          <StudentMainContent />
+          <StudentMainContent />
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default StudentDashboard
