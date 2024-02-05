a44a2b0e31a69c9c8911ae851a2d282d432710d8
Rakhshan khan <rakshaankhan1@gmail.com>
2024-02-05 23:46:37 +0530

    Working on attendence page

--- src/Components/Student/StudentAttendence/StudentAttendence.jsx
+++ src/Components/Student/StudentAttendence/StudentAttendence.jsx
@@ -2,24 +2,113 @@ import React from 'react'
 import './StudentAttendence.css'
 
 function StudentAttendance() {
-  const Attendence = {        /// data will fetch by api from backend
-    Jan: [true,false,true],
-    fab: [false]
-  }
+  
+
   const sessionArray = [2000,2001,2001]
 
   const sessions = sessionArray.map((session)=>{
      return <option value={session}>{session}</option>
   })
 
-  const JanAttendence = Attendence.Jan.map((day)=>{
-    return (day==true )? <td>P</td> : <td>A</td>
-  })
-
+  const Attendence = [
+    {
+      monthName: 'January',
+      days: {
+        1: false,
+        2: true,
+        3: false,
+        4: true,
+        5: false,
+        6: true,
+        7: false,
+        8: true,
+        9: true,
+        10: true,
+        11: false,
+        12: false,
+        13: true,
+        14: false,
+        15: true,
+        16: true,
+        17: true,
+        18: false,
+        19: true,
+        20: false,
+        21: true,
+        22: false,
+        23: true,
+        24: false,
+        25: true,
+        26: false,
+        27: false,
+        28: true,
+        29: true,
+        30: true,
+        31: false,
+      },
+    },
+    {
+      monthName: 'Fabruary',
+      days: {
+        1: false,
+        2: true,
+        3: false,
+        4: true,
+        5: false,
+        6: true,
+        7: false,
+        8: true,
+        9: true,
+        10: true,
+        11: false,
+        12: false,
+        13: true,
+        14: false,
+        15: true,
+        16: true,
+        17: true,
+        18: false,
+        19: true,
+        20: false,
+        21: true,
+        22: false,
+        23: true,
+        24: false,
+        25: true,
+        26: false,
+        27: false,
+        28: true,
+        29: true,
+        30: true,
+        31: false,
+      },
+    },
+  ]
  
   const date = Array.from({ length: 31 }, (_, index) => (
     <th key={index + 1}>{index + 1}</th>
   ))
+
+  const monthAttendence = Attendence.map((month) => {
+    let totalPresent = 0;
+    let totalAbsent = 0;
+    return (
+      <tr>
+        <td>{month.monthName}</td>
+        {
+          Object.values(month.days).map((day)=>{
+            day == true ? totalPresent+1 : totalAbsent+1
+            return (
+              day == true ? <td>P</td> : <td>A</td>
+              )
+          })
+        }
+        <td>{totalPresent}</td>
+      </tr>
+    )
+    
+    
+  })
 
   return (
     <div>
@@ -41,44 +130,7 @@ function StudentAttendance() {
           <th>Total</th>
         </thead>
         <tbody>
-          <tr>
-            <td>January</td>
-            {JanAttendence}
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>February</td>
-          </tr>
-          <tr>
-            <td>March</td>
-          </tr>
-          <tr>
-            <td>Aprail</td>
-          </tr>
-          <tr>
-            <td>May</td>
-          </tr>
-          <tr>
-            <td>June</td>
-          </tr>
-          <tr>
-            <td>July</td>
-          </tr>
-          <tr>
-            <td>August</td>
-          </tr>
-          <tr>
-            <td>September</td>
-          </tr>
-          <tr>
-            <td>October</td>
-          </tr>
-          <tr>
-            <td>November</td>
-          </tr>
-          <tr>
-            <td>December</td>
-          </tr>
+          {monthAttendence}
         </tbody>
       </table>
     </div>
